f1356db60226dc67ef1ef2de0425fad0d5c79349
anonymous <89799912+PRATHEEsni@users.noreply.github.com>
2021-09-20 12:08:17 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,7 +9,7 @@ async def start(_, message: Message):
         text="**Hello 👋🏻 {}!**\n\nI **Can Play Music In Voice Chats of Telegram Groups.**I Have A **lot of cool feature that will amaze You!**\n\n**Click /cmdlist For More Help On My Usage ❤**".format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton("➕ Add To Your Group ➕", url=https://t.me/Ethix_Musical_Bot?startgroup=true)
+            InlineKeyboardButton("➕ Add To Your Group ➕", url="https://t.me/Ethix_Musical_Bot?startgroup=true")
             ],[
             InlineKeyboardButton("💬 Support Group", url="https://t.me/SHIZUKA_VC_SUPPORT"),
             InlineKeyboardButton("DEV 🥀", url="https://t.me/pratheek06")
